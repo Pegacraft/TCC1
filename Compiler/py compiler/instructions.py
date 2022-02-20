173931bc19a334b937f6e6b96e80e4d643290a42
Pegacraffft <karl.w.schramm@gmail.com>
2022-02-20 04:09:23 +0100

    added instructions to the py compiler

--- Compiler/py compiler/instructions.py
+++ Compiler/py compiler/instructions.py
@@ -1,35 +1,77 @@
-def create_opcodes(instruction: str, param1: int, param2: int, param3: int):
+def create_opcodes(instruction: str, r: int, s: int, t: int):
     instructions: dict = {
+        """
+        DICT STRUCTURE
+        [opcode name]: tuple
+        The tuple can contain an int or an tuple:
+           - int: gets appended directly onto the byte array
+           - tuple: ([value], [size])
+                value: the value that should be appended to the array
+                size: how many bytes it will occupy
+                example -> (0x01, 2) would return b'\x00\x01'
+        """
         # Unconditional jmp/branch
-        "jmp": (0x01, (param1, 7)),
-        "b": (0x09, (param1, 7)),
+        "jmp": (0x01, (r, 7)),
+        "b": (0x09, (r, 7)),
         # Conditional jmp/branch
-        "jeq": (0x02, (param1, 1), (param2, 1), 0x00, (param3, 4)),
-        "beq": (0x0A, (param1, 1), (param2, 1), 0x00, (param3, 4)),
-        "jneq": (0x03, (param1, 1), (param2, 1), 0x00, (param3, 4)),
-        "bneq": (0x0B, (param1, 1), (param2, 1), 0x00, (param3, 4)),
-        "jl": (0x04, (param1, 1), (param2, 1), 0x00, (param3, 4)),
-        "bl": (0x0C, (param1, 1), (param2, 1), 0x00, (param3, 4)),
-        "jleq": (0x05, (param1, 1), (param2, 1), 0x00, (param3, 4)),
-        "bleq": (0x0D, (param1, 1), (param2, 1), 0x00, (param3, 4)),
-        "jg": (0x06, (param1, 1), (param2, 1), 0x00, (param3, 4)),
-        "bg": (0x0E, (param1, 1), (param2, 1), 0x00, (param3, 4)),
-        "jgeq": (0x07, (param1, 1), (param2, 1), 0x00, (param3, 4)),
-        "bgeq": (0x0F, (param1, 1), (param2, 1), 0x00, (param3, 4)),
+        "jeq": (0x02, (r, 1), (s, 1), 0x00, (t, 4)),
+        "beq": (0x0A, (r, 1), (s, 1), 0x00, (t, 4)),
+        "jneq": (0x03, (r, 1), (s, 1), 0x00, (t, 4)),
+        "bneq": (0x0B, (r, 1), (s, 1), 0x00, (t, 4)),
+        "jl": (0x04, (r, 1), (s, 1), 0x00, (t, 4)),
+        "bl": (0x0C, (r, 1), (s, 1), 0x00, (t, 4)),
+        "jleq": (0x05, (r, 1), (s, 1), 0x00, (t, 4)),
+        "bleq": (0x0D, (r, 1), (s, 1), 0x00, (t, 4)),
+        "jg": (0x06, (r, 1), (s, 1), 0x00, (t, 4)),
+        "bg": (0x0E, (r, 1), (s, 1), 0x00, (t, 4)),
+        "jgeq": (0x07, (r, 1), (s, 1), 0x00, (t, 4)),
+        "bgeq": (0x0F, (r, 1), (s, 1), 0x00, (t, 4)),
         # With immediate:
-        "jeqi": (0x12, (param1, 1), (param3, 2), (param2, 4)),
-        "beqi": (0x1A, (param1, 1), (param3, 2), (param2, 4)),
-        "jneqi": (0x13, (param1, 1), (param3, 2), (param2, 4)),
-        "bneqi": (0x1B, (param1, 1), (param3, 2), (param2, 4)),
-        "jli": (0x14, (param1, 1), (param3, 2), (param2, 4)),
-        "bli": (0x1C, (param1, 1), (param3, 2), (param2, 4)),
-        "jleqi": (0x15, (param1, 1), (param3, 2), (param2, 4)),
-        "bleqi": (0x1D, (param1, 1), (param3, 2), (param2, 4)),
-        "jgi": (0x16, (param1, 1), (param3, 2), (param2, 4)),
-        "bgi": (0x1E, (param1, 1), (param3, 2), (param2, 4)),
-        "jgeqi": (0x17, (param1, 1), (param3, 2), (param2, 4)),
-        "bgeqi": (0x1F, (param1, 1), (param3, 2), (param2, 4)),
+        "jeqi": (0x12, (r, 1), (t, 2), (s, 4)),
+        "beqi": (0x1A, (r, 1), (t, 2), (s, 4)),
+        "jneqi": (0x13, (r, 1), (t, 2), (s, 4)),
+        "bneqi": (0x1B, (r, 1), (t, 2), (s, 4)),
+        "jli": (0x14, (r, 1), (t, 2), (s, 4)),
+        "bli": (0x1C, (r, 1), (t, 2), (s, 4)),
+        "jleqi": (0x15, (r, 1), (t, 2), (s, 4)),
+        "bleqi": (0x1D, (r, 1), (t, 2), (s, 4)),
+        "jgi": (0x16, (r, 1), (t, 2), (s, 4)),
+        "bgi": (0x1E, (r, 1), (t, 2), (s, 4)),
+        "jgeqi": (0x17, (r, 1), (t, 2), (s, 4)),
+        "bgeqi": (0x1F, (r, 1), (t, 2), (s, 4)),
 
+        # ALU operations
+        "add": (0x80, (r, 1), (s, 1), (t, 1), (0, 4)),
+        "addi": (0x90, (r, 1), 0x00, (t, 1), (s, 4)),
+        "sub": (0x81, (r, 1), (s, 1), (t, 1), (0, 4)),
+        "subi": (0x91, (r, 1), 0x00, (t, 1), (s, 4)),
+        "mul": (0x82, (r, 1), (s, 1), (t, 1), (0, 4)),
+        "muli": (0x92, (r, 1), 0x00, (t, 1), (s, 4)),
+        "and": (0x83, (r, 1), (s, 1), (t, 1), (0, 4)),
+        "andi": (0x93, (r, 1), 0x00, (t, 1), (s, 4)),
+        "or": (0x84, (r, 1), (s, 1), (t, 1), (0, 4)),
+        "ori": (0x94, (r, 1), 0x00, (t, 1), (s, 4)),
+        "not": (0x85, (r, 1), 0x00, (t, 1), (0, 4)),
+        "xor": (0x86, (r, 1), (s, 1), (t, 1), (0, 4)),
+        "xori": (0x96, (r, 1), 0x00, (t, 1), (t, 4)),
+        "xnor": (0x87, (r, 1), (s, 1), (t, 1), (0, 4)),
+        "xnori": (0x97, (r, 1), 0x00, (t, 1), (t, 4)),
+        "nor": (0x88, (r, 1), (s, 1), (t, 1), (0, 4)),
+        "nori": (0x98, (r, 1), 0x00, (t, 1), (t, 4)),
+        "ceq": (0x89, (r, 1), (s, 1), (t, 1), (0, 4)),
+        "ceqi": (0x99, (r, 1), 0x00, (t, 1), (t, 4)),
+        "clt": (0x8A, (r, 1), (s, 1), (t, 1), (0, 4)),
+        "clti": (0x9A, (r, 1), 0x00, (t, 1), (t, 4)),
+        "clte": (0x8B, (r, 1), (s, 1), (t, 1), (0, 4)),
+        "cltei": (0x9B, (r, 1), 0x00, (t, 1), (t, 4)),
+        "shr": (0x8C, (r, 1), (s, 1), (t, 1), (0, 4)),
+        "shri": (0x9C, (r, 1), 0x00, (t, 1), (t, 4)),
+        "shl": (0x8D, (r, 1), (s, 1), (t, 1), (0, 4)),
+        "shli": (0x9D, (r, 1), 0x00, (t, 1), (t, 4)),
+        "rr": (0x8E, (r, 1), (s, 1), (t, 1), (0, 4)),
+        "rri": (0x9E, (r, 1), 0x00, (t, 1), (t, 4)),
+        "rl": (0x8F, (r, 1), (s, 1), (t, 1), (0, 4)),
+        "rli": (0x9F, (r, 1), 0x00, (t, 1), (t, 4)),
     }
 
     instruction_cmd: tuple = instructions.get(instruction)
--- Compiler/py compiler/main.py
+++ Compiler/py compiler/main.py
@@ -1,6 +1,9 @@
 import instructions
 
-test: bytearray = instructions.create_opcodes("jli", 1, 2, 3)
+test: bytearray = instructions.create_opcodes("clti", 1, 2, 3)
 
+text: str = ""
 for i in test:
-    print(hex(i))
+    text += hex(i) + " "
+
+print(text)
