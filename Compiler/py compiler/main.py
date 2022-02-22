b147a10f90c4132050efece69703756a83139b43
Peppy3 <lindfors.freja@gmail.com>
2022-02-22 14:25:22 +0100

    py compiler updates

--- Compiler/py compiler/main.py
+++ Compiler/py compiler/main.py
@@ -5,5 +5,6 @@ test: bytearray = instructions.create_opcodes("cpeq", 1, 2, 3, 4)
 text: str = ""
 for i in test:
     text += hex(i) + " "
-
+#with open('a.bin', 'w+b') as f:
+    #f.write(test)
 print(text)
